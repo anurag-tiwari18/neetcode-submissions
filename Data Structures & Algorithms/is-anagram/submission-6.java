c3e186efe7502e59fe0c1ef38df4de7cca87fb69
Anurag Tiwari <225207128+anurag-tiwari18@users.noreply.github.com>
2026-06-08 18:09:29 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        HashMap<Character,Integer> mp1 = new HashMap<>();
+        HashMap<Character,Integer> mp2 = new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            mp1.put(s.charAt(i),mp1.getOrDefault(s.charAt(i),0)+1);
+            mp2.put(t.charAt(i),mp2.getOrDefault(t.charAt(i),0)+1);
+        }           
+
+        for(int i=0;i<s.length();i++){
+            if(!mp1.get(s.charAt(i)) .equals (mp2.get(s.charAt(i)))) return false;
+        }
+        return true;
+    }
+}
